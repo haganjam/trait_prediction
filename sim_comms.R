897023bc359371b0b6dbdd8a455ea12d83f63b6f
James Hagan <james_hagan@outlook.com>
2022-05-20 15:20:44 +0200

    update the code to draw random communities from a regional species pool

--- sim_comms.R
+++ sim_comms.R
@@ -1,6 +1,134 @@
 
 # Simulating predictive value of traits
 
+# How to simulate random communities?
+
+# What is the theoretical limit to the correlation between traits and ecosystem processes
+
+# load relevant libraries
+library(mobsim)
+library(vegan)
+library(betapart)
+
+# function to simulate a regional species pool: Sim_regional
+
+# args
+
+#' @param nspec - number of species in the regional species pool
+#' @param nind - number of individuals in the regional species pool (should be a very large number like 100000) 
+#' @param sad - type of species abundance distribution (default lognormal but see mobsim for details)
+#' @param plot - T or F whether to plot the histogram of abundance distributions
+
+Sim_regional <- function(nspec = 10, nind = 100000, plot = TRUE) {
+  
+  # simulate a species pool
+  reg_vector <- sim_sad(s_pool = nspec, n_sim = nind, sad_type = "lnorm")
+  attr(reg_vector, "class") <- NULL
+  names(reg_vector) <- NULL
+  
+  # add species names to these species
+  specnames <- paste("sp", 1:length(reg_vector), sep = "")
+  names(reg_vector) <- specnames
+  
+  # plot a histogram of the regional species pool abundance distribution
+  if (plot) {
+    hist(reg_vector)
+  }
+  
+  # add a species number attribute to the reg_vector
+  attr(reg_vector, "specnumber") <- nspec
+  
+  return(reg_vector)
+  
+}
+
+# function to sample from a regional pool vector from Sim_regional: Reg_sampler
+
+# args
+
+#' @param reg_vector - regional species pool vector from Reg_sampler 
+#' @param Kmean - average carrying capacity i.e. number of individuals that a patch can support (varies with a Poisson distribution)
+
+Reg_sampler <- function(reg_vector,  Kmean = 100) {
+  
+  # get the specnames variable from the regional pool
+  specnames <- attr(reg_vector, "specnumber")
+  
+  # make a list of specnames to serve as the base
+  specnames <- paste("sp", 1:specnames, sep = "")
+  
+  # draw a sample from the distribution
+  x <- sample(rep(names(reg_vector), reg_vector), size = rpois(n = 1, Kmean), replace = TRUE )
+  x <- table(x)
+  
+  # extract the missing names and get an ordered list of missing names and present names
+  missingnames <- specnames[!specnames %in% names(x)]
+  fullnames <- c(names(x), missingnames)
+  
+  # add zeros for the missing names and add the missing names
+  names(x) <- NULL
+  x <- c(x, rep(0, length(missingnames) ))
+  names(x) <- fullnames
+  x <- x[sort(names(x))]
+  
+  # return the sample
+  return(x)
+  
+}
+
+# function to simulate n independent random samples from a regional species pool: Sim_comms
+
+# args
+
+#' @param reg_vector - regional species pool vector from Reg_sampler 
+#' @param Kmean - average carrying capacity i.e. number of individuals that a patch can support (varies with a Poisson distribution)
+#' @param ncomm - number of independent communities to sample 
+#' @param dom_variation - TRUE of FALSE: highest abundance species identity varies across communities
+
+Sim_comms <- function(reg_vector, Kmean = 100, ncomm = 5, dom_variation = FALSE) {
+  
+  y <- 
+    lapply(1:ncomm, function(vec) {
+      x <- Reg_sampler(reg_vector = reg_vector, Kmean = Kmean)
+      return(x)
+    })
+  
+  if (dom_variation) {
+    y <- 
+      lapply(y, function(z) {
+        maxin <- max(z)
+        maxid <- which(z == maxin)
+        spid <- 1:length(z)
+        sid <- sample(spid[!(spid %in% maxid)], 1)
+        z[maxid] <- z[sid]
+        z[sid] <- maxin
+        return(z)
+      })
+  }
+  
+  return(y)
+  
+}
+
+# set the number of species to simulate
+nspec <- 30
+ncomm <- 1
+
+# simulate a set of species traits
+traits.m <- round(rnorm(n = nspec, mean = 10, 3), 1)
+names(traits) <- paste("sp", 1:nspec, sep = "")
+
+# simulate a regional pool of species
+reg_vector <- Sim_regional(nspec = nspec, nind = 100000, plot = TRUE)
+
+# test the sim_comms function
+Sim_comms(reg_vector = reg_vector, Kmean = 100, ncomm = ncomm , dom_variation = TRUE)
+
+
+
+
+
+
 # maybe we need I need a bit more reading
 
 # 1. simple model showing that the way we sample can strongly affect our quantification of CWM and FD
